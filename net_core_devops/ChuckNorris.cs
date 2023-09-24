0384c4a0662e84eda620e488f28c9d6d4705eaca
igorleessa <igorleessa@gmail.com>
2023-09-24 18:22:19 -0300

    Adicionar arquivos de projeto.

---
+++ net_core_devops/ChuckNorris.cs
@@ -0,0 +1,11 @@
+namespace net_core_devops
+{
+    public class ChuckNorris
+    {
+        public List<string> categories { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string url{ get; set; }
+        public string value { get; set; }
+    }
+}
